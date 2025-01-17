010006fc43a59c2503f327928b3623648a6ab387
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-01-18 04:44:29 +0530

    header

--- atdigital/src/components/Header.jsx
+++ atdigital/src/components/Header.jsx
@@ -9,19 +9,20 @@ const Header = () => {
       id="Header"
     >
       <TopNavbar />
-      <div className="fixed w-[630px] h-[306px] top-[354px] left-[80px] px-[40px] pt-[24px] pb-[32px] gap-[20px] bg-gradient-to-r from-[#4DCA79] to-[#1CBDDD] container mx-auto">
-        <div className="w-[556px] h-[192px]">
-        <h1 className="text-[48px] font-[700] leading-[48px] tracking-[-0.02em] text-left text-white no-underline decoration-transparent">
-          We Crush Your Competitors, Goals, And Sales Records - Without The B.S.
-        </h1>
+      <div className="absolute top-[354px] left-[60px] w-[622px] h-[306px] px-[40px] pt-[24px] pb-[32px] gap-[20px] bg-gradient-to-r from-[#4DCA79] to-[#1CBDDD] mx-auto opacity-100 
+       md:h-[258px]">
+        <div className="w-[556px] h-[192px] p-">
+          <h1 className="text-[48px] font-[700] leading-[48px] tracking-[-0.02em] text-left text-white">
+            We Crush Your Competitors, Goals, And Sales Records - Without The
+            B.S.
+          </h1>
         </div>
         <button
-  className="mt-4 w-[214px] h-[38px] px-[20px] py-[12px] gap-[8px] rounded-tl-[4px] rounded-tr-[0px] rounded-bl-[0px] rounded-br-[0px] bg-[#F28D35] text-white font-[700] text-[14px] leading-[14px] tracking-[-0.02em] text-left no-underline decoration-transparent"
-  aria-label="Get a free consultation"
->
-  GET FREE CONSULTATION
-</button>
-
+          className="mt-4 w-[214px] h-[38px] px-[20px] py-[12px] gap-[8px] rounded-tl-[4px] rounded-tr-[0px] rounded-bl-[0px] rounded-br-[0px] bg-[#F28D35] text-white font-[700] text-[14px] leading-[14px] tracking-[-0.02em] text-left no-underline decoration-transparent"
+          aria-label="Get a free consultation"
+        >
+          GET FREE CONSULTATION
+        </button>
       </div>
     </div>
   );
--- atdigital/src/components/TopNavbar.jsx
+++ atdigital/src/components/TopNavbar.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 
 const TopNavbar = () => {
+
+  const [showmobileMenu, setShowMobileMenu] = useState(false)
+
+  useEffect(()=>{
+    if (showmobileMenu) {
+      document.body.style.overflow= 'hidden'
+    } else {
+      document.body.style.overflow='auto'
+    }
+    return ()=>{
+      document.body.style.overflow ='auto'
+    }
+  },[showmobileMenu])
+
   return (
-    <div className='absolute top-0 left-0 w-full z-10 bg-primary'>
+    <div className='absolute top-0 left-0 w-full bg-primary'>
         <div className='container mx-auto flex justify-between items-center py-6'>
             <img src={assets.logo} alt="" />
             <ul className='hidden md:flex gap-7 text-white'>
-                <a href="#services" className='cursor-pointer hover:text-gray-400'>SERVICES</a>
-                <a href="#about" className='cursor-pointer hover:text-gray-400'>ABOUT US</a>
-                <a href="#contact" className='cursor-pointer hover:text-gray-400'>CONTACT US</a>
-                <a href="#careers" className='cursor-pointer hover:text-gray-400'>CAREERS</a>
+                <a onClick={()=> setShowMobileMenu(false)} href="#services" className='cursor-pointer hover:text-gray-400'>SERVICES</a>
+                <a onClick={()=> setShowMobileMenu(false)} href="#about" className='cursor-pointer hover:text-gray-400'>ABOUT US</a>
+                <a onClick={()=> setShowMobileMenu(false)} href="#contact" className='cursor-pointer hover:text-gray-400'>CONTACT US</a>
+                <a onClick={()=> setShowMobileMenu(false)} href="#careers" className='cursor-pointer hover:text-gray-400'>CAREERS</a>
             </ul>
+            <img onClick={()=> setShowMobileMenu(true)} src={assets.menu} alt=""  className='sm:hidden w-7 cursor-pointer'/>
+        </div>
+
+        {/* mobile-menu */}
+        <div className={`sm:hidden ${showmobileMenu ? ' fixed w-full' :'h-0 w-0' } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
+          <div className='flex justify-end p-6 cursor-pointer'>
+            <img onClick={()=> setShowMobileMenu(false)} src={assets.close} alt="" className='w-6'/>
+          </div>
+          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+            <a href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+            <a href="#services" className='px-4 py-2 rounded-full inline-block'>SERVICES</a>
+            <a href="#about" className='px-4 py-2 rounded-full inline-block'>ABOUT US</a>
+            <a href="#contact" className='px-4 py-2 rounded-full inline-block'>CONTACT US</a>
+            <a href="#careers" className='px-4 py-2 rounded-full inline-block'>CAREERS</a>
+          </ul>
         </div>
     </div>
   )
