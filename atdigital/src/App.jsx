6fb3de2a420b860a5372f8451a9e770ca2e06fca
dilmiishara2002 <dilmiishara2000@gmail.com>
2025-01-19 01:57:19 +0530

    changed styles

--- atdigital/src/App.jsx
+++ atdigital/src/App.jsx
@@ -8,64 +8,66 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="w-full overflow-hidden">
+    <div className="w-full overflow-hidden pt-20 ">
       <Header />
 
-      <main className="bg-white">
-        <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 relative">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            {/* Image section with fixed width and height */}
+      <main className="bg-white pt-5 sm:pt-30 sm:flex sm:flex-col sm:items-center sm:justify-center">
+        <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 relative sm:mt-20 sm:items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 sm:gap-5">
+            {/* Image section with flexible width and height */}
             <div className="flex justify-center">
               <img
-                src={assets.image1}
+                src={assets.image2}
                 alt="Web & Mobile App Development"
-                className="w-[414px] h-[414px] object-cover"
+                className="object-cover sm:h-[276px] sm:w-auto"
               />
             </div>
 
             {/* Content section (h2 and p in two columns) */}
-            <div className="flex flex-col justify-center">
-              <h2 className="text-3xl font-bold text-primary mb-6">
+            <div className="flex flex-col justify-center sm:items-center xl:w-[542px] lg:w-[542px] md:w-[542px]">
+              <h2 className="text-3xl font-bold text-primary mb-6 sm:text-center md:text-left lg:text-left xl:text-left">
                 Web & Mobile App Development
               </h2>
-              <p className="text-gray-600">
+              <p className="text-gray-600 sm:text-center md:text-left lg:text-left xl:text-left  mb-5">
                 Your web and mobile Apps are pieces of the puzzle to grow your
                 business. We use frameworks that tailor content and engagement
                 methods to respond to different intents shown by your potential
                 customers.
               </p>
-              <Button text="LEARN MORE" />
+              <Button text="LEARN MORE" className="sm:mx-auto sm:mt-4" />{" "}
             </div>
           </div>
         </section>
-        <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            {/* Image section with fixed width and height */}
-          
+
+        <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative sm:items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 sm:gap-5">
             {/* Content section (h2 and p in two columns) */}
-            <div className="flex flex-col justify-center">
-              <h2 className="text-3xl font-bold text-primary mb-6">
-              Digital Strategy Consulting
+            <div className="flex flex-col justify-center sm:items-center xl:w-[542px] lg:w-[542px] md:w-[542px]">
+              <h2 className="text-3xl font-bold text-primary mb-6 sm:text-center md:text-left lg:text-left xl:text-left">
+                Digital Strategy Consulting
               </h2>
-              <p className="text-gray-600">
-              Your digital strategy should complement the overall marketing
-            strategy. We provide a clear concept and strategic overview to find
-            the most efficient model for your business.
+              <p className="text-gray-600 sm:text-center md:text-left lg:text-left xl:text-left mb-5">
+                Your digital strategy should complement the overall marketing
+                strategy. We provide a clear concept and strategic overview to
+                find the most efficient model for your business.
               </p>
-              <Button text="LEARN MORE" />
+              <Button text="LEARN MORE" className="sm:mx-auto sm:mt-5" />
             </div>
-            <div className="flex justify-center">
+
+            {/* Image section with flexible width and height */}
+            <div className="flex justify-center sm:order-first lg:order-last xl:order-last md:order-last">
               <img
-                src={assets.image2}
-                alt="Web & Mobile App Development"
-                className="w-[414px] h-[414px] object-cover"
+                src={assets.image1}
+                alt="Digital Strategy Consulting"
+                className="object-cover sm:h-[276px] sm:w-auto"
               />
             </div>
           </div>
         </section>
       </main>
-      <FAQ/>
-      <Footer/>
+
+      <FAQ />
+      <Footer />
     </div>
   );
 };
--- atdigital/src/components/TopNavbar.jsx
+++ atdigital/src/components/TopNavbar.jsx
@@ -1,49 +1,100 @@
-import React, { useEffect, useState } from 'react'
-import { assets } from '../assets/assets'
+import React, { useEffect, useState } from "react";
+import { assets } from "../assets/assets";
 
 const TopNavbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-  const [showmobileMenu, setShowMobileMenu] = useState(false)
-
-  useEffect(()=>{
-    if (showmobileMenu) {
-      document.body.style.overflow= 'hidden'
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow='auto'
+      document.body.style.overflow = "auto";
     }
-    return ()=>{
-      document.body.style.overflow ='auto'
-    }
-  },[showmobileMenu])
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showMobileMenu]);
 
   return (
-    <div className='absolute top-0 left-0 w-full bg-primary'>
-        <div className='container mx-auto flex justify-between items-center py-6'>
-            <img src={assets.logo} alt="" />
-            <ul className='hidden md:flex gap-7 text-white'>
-                <a onClick={()=> setShowMobileMenu(false)} href="#services" className='cursor-pointer hover:text-gray-400'>SERVICES</a>
-                <a onClick={()=> setShowMobileMenu(false)} href="#about" className='cursor-pointer hover:text-gray-400'>ABOUT US</a>
-                <a onClick={()=> setShowMobileMenu(false)} href="#contact" className='cursor-pointer hover:text-gray-400'>CONTACT US</a>
-                <a onClick={()=> setShowMobileMenu(false)} href="#careers" className='cursor-pointer hover:text-gray-400'>CAREERS</a>
-            </ul>
-            <img onClick={()=> setShowMobileMenu(true)} src={assets.menu} alt=""  className='sm:hidden w-7 cursor-pointer'/>
-        </div>
+    <div className="absolute top-0 left-0 w-full bg-primary md:h-[101px] sm:h-[85px]">
+      <div className="container mx-auto flex justify-between items-center py-6 px-4 lg:px-10">
+        <img
+          src={assets.logo}
+          alt="Logo"
+          className="w-auto h-[40px] md:h-[50px]"
+        />
+        <ul className="hidden md:flex gap-4 lg:gap-7 text-white">
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#services"
+            className="cursor-pointer hover:text-gray-400"
+          >
+            SERVICES
+          </a>
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#about"
+            className="cursor-pointer hover:text-gray-400"
+          >
+            ABOUT US
+          </a>
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#contact"
+            className="cursor-pointer hover:text-gray-400"
+          >
+            CONTACT US
+          </a>
+          <a
+            onClick={() => setShowMobileMenu(false)}
+            href="#careers"
+            className="cursor-pointer hover:text-gray-400"
+          >
+            CAREERS
+          </a>
+        </ul>
+        <img
+          onClick={() => setShowMobileMenu(true)}
+          src={assets.menu}
+          alt="Menu Icon"
+          className="sm:hidden w-7 cursor-pointer"
+        />
+      </div>
 
-        {/* mobile-menu */}
-        <div className={`sm:hidden ${showmobileMenu ? ' fixed w-full' :'h-0 w-0' } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
-          <div className='flex justify-end p-6 cursor-pointer'>
-            <img onClick={()=> setShowMobileMenu(false)} src={assets.close} alt="" className='w-6'/>
-          </div>
-          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
-            <a href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
-            <a href="#services" className='px-4 py-2 rounded-full inline-block'>SERVICES</a>
-            <a href="#about" className='px-4 py-2 rounded-full inline-block'>ABOUT US</a>
-            <a href="#contact" className='px-4 py-2 rounded-full inline-block'>CONTACT US</a>
-            <a href="#careers" className='px-4 py-2 rounded-full inline-block'>CAREERS</a>
-          </ul>
+      {/* Mobile menu */}
+      <div
+        className={`sm:hidden ${
+          showMobileMenu ? "fixed w-full" : "h-0 w-0"
+        } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <div className="flex justify-end p-6 cursor-pointer">
+          <img
+            onClick={() => setShowMobileMenu(false)}
+            src={assets.close}
+            alt="Close Icon"
+            className="w-6"
+          />
         </div>
+        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+          <a href="#Header" className="px-4 py-2 rounded-full inline-block">
+            Home
+          </a>
+          <a href="#services" className="px-4 py-2 rounded-full inline-block">
+            SERVICES
+          </a>
+          <a href="#about" className="px-4 py-2 rounded-full inline-block">
+            ABOUT US
+          </a>
+          <a href="#contact" className="px-4 py-2 rounded-full inline-block">
+            CONTACT US
+          </a>
+          <a href="#careers" className="px-4 py-2 rounded-full inline-block">
+            CAREERS
+          </a>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopNavbar
+export default TopNavbar;
